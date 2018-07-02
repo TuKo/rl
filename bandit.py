89186085d78b5df98f670f2e28d682448e107ac0
Javier Turek <javier.turek@intel.com>
2018-07-02 14:50:54 -0700

    Added bandit testbed

---
+++ bandit.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class Bandit(object):
+
+    def __init__(self, k=10, mean=0.0, std=1.0):
+        self._arms = k
+        self._mean = mean
+        self._std = std
+
+        self._q_star = np.random.normal(loc=mean, scale=std, size=k)
+
+    def reward(self, action):
+        return np.random.normal(self._q_star[action], scale=self._std, size=1)
+
